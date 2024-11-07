1f37d60e02b63430b143ebe739c5d5528acc6256
BurykNazar <buryk.nazar@dspu.edu.ua>
2024-11-07 16:23:55 +0200

    Lab2

---
+++ Lab_2/Source.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+struct PQR {
+	bool p = false;
+	bool q = false;
+	bool r = false;
+};
+
+bool endFuncHandle(PQR pqr) {
+	return pqr.p != true && pqr.q != true && pqr.r != true;
+}
+
+bool checkXOR(PQR pqr) {
+	return pqr.q != pqr.r;
+}
+
+bool checkAND(PQR pqr) {
+	return pqr.p && checkXOR(pqr);
+}
+
+void displayTable() {
+	PQR pqr;
+	cout << "Таблиця істинності для функції p AND (q XOR r):" << endl;
+	cout << "_________________________________________" << endl;
+	cout << "| p | q | r | q XOR r | p AND (q XOR r) |" << endl;
+	cout << "|___|___|___|_________|_________________|" << endl;
+
+	for (int i = 7; i >= 0; i--) {
+		pqr.p = i & 4;
+		pqr.q = i & 2;
+		pqr.r = i & 1;
+
+		bool xorResult = checkXOR(pqr);
+		bool finalResult = pqr.p && xorResult;
+
+		cout << "| " << pqr.p << " | " << pqr.q << " | " << pqr.r << " |";
+		cout << "   " << xorResult << "     |       " << finalResult << "         |" << endl;
+		cout << "|___|___|___|_________|_________________|" << endl;
+	}
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	displayTable();
+
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,82 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab_2\Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestXOR_True)
+		{
+			PQR pqr;
+			pqr.q = true;
+			pqr.r = false;
+
+			bool result = checkXOR(pqr);
+
+			Assert::IsTrue(result);
+		}
+
+		TEST_METHOD(TestXOR_False)
+		{
+			PQR pqr;
+			pqr.q = true;
+			pqr.r = true;
+
+			bool result = checkXOR(pqr);
+
+			Assert::IsFalse(result);
+		}
+
+		TEST_METHOD(TestAND_True)
+		{
+			PQR pqr;
+			pqr.p = true;
+			pqr.q = true;
+			pqr.r = false;
+
+			bool result = checkAND(pqr);
+
+			Assert::IsTrue(result);
+		}
+
+		TEST_METHOD(TestAND_False)
+		{
+			PQR pqr;
+			pqr.p = false;
+			pqr.q = true;
+			pqr.r = false;
+
+			bool result = checkAND(pqr);
+
+			Assert::IsFalse(result);
+		}
+
+		TEST_METHOD(TestEndFuncHandle_True)
+		{
+			PQR pqr;
+			pqr.p = false;
+			pqr.q = false;
+			pqr.r = false;
+
+			bool result = endFuncHandle(pqr);
+
+			Assert::IsTrue(result);
+		}
+
+		TEST_METHOD(TestEndFuncHandle_False)
+		{
+			PQR pqr;
+			pqr.p = true;
+			pqr.q = false;
+			pqr.r = false;
+
+			bool result = endFuncHandle(pqr);
+
+			Assert::IsFalse(result);
+		}
+	};
+}
